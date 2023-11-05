bb985d4f7c419e866878bb2ecfb0c456a709de93
Lucy Poulton <lucy@lucypoulton.net>
2023-11-05 17:57:52 +0000

    Delete everything to give me a diff to work with

--- src/main/kotlin/net/topstrix/hubinteractions/HubInteractionsPlugin.kt
+++
@@ -1,9 +0,0 @@
-package net.topstrix.hubinteractions
-
-import org.bukkit.plugin.java.JavaPlugin
-
-class HubInteractionsPlugin: JavaPlugin() {
-    override fun onEnable() {
-        HubInteractions.onEnable(this)
-    }
-}
