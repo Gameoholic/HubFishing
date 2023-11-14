7cbf6425a130cc68dfd916747ee42e4188507bfb
Game <viner.atmic@gmail.com>
2023-11-14 13:43:51 +0200

    refactor: use UseSerializers file annotation in SoundsConfig

--- src/main/kotlin/xyz/gameoholic/hubfishing/config/SoundsConfig.kt
+++ src/main/kotlin/xyz/gameoholic/hubfishing/config/SoundsConfig.kt
@@ -1,18 +1,22 @@
+@file:UseSerializers(SoundSerializer::class)
+
 package xyz.gameoholic.hubfishing.config
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.UseSerializers
 import net.kyori.adventure.sound.Sound
 import xyz.gameoholic.hubfishing.serialization.SoundSerializer
 
 @Serializable
+
 data class SoundsConfig(
-    @SerialName("time-restriction-strike-sound") val timeRestrictionStrikeSound: @Serializable(with = SoundSerializer::class) Sound,
-    @SerialName("minigame-failure-sound") val minigameFailureSound: @Serializable(with = SoundSerializer::class) Sound,
-    @SerialName("passed-time-restriction-sound") val passedTimeRestrictionSound: @Serializable(with = SoundSerializer::class) Sound,
-    @SerialName("fish-found-sound") val fishFoundSound: @Serializable(with = SoundSerializer::class) Sound,
-    @SerialName("fishing-minigame-miss-sound") val fishingMinigameMissSound: @Serializable(with = SoundSerializer::class) Sound,
-    @SerialName("level-up-sound") val levelUpSound: @Serializable(with = SoundSerializer::class) Sound,
-    @SerialName("fishing-minigame-catch-sound") val fishingMinigameCatchSound: @Serializable(with = SoundSerializer::class) Sound,
+    @SerialName("time-restriction-strike-sound") val timeRestrictionStrikeSound: Sound,
+    @SerialName("minigame-failure-sound") val minigameFailureSound: Sound,
+    @SerialName("passed-time-restriction-sound") val passedTimeRestrictionSound: Sound,
+    @SerialName("fish-found-sound") val fishFoundSound: Sound,
+    @SerialName("fishing-minigame-miss-sound") val fishingMinigameMissSound: Sound,
+    @SerialName("level-up-sound") val levelUpSound: Sound,
+    @SerialName("fishing-minigame-catch-sound") val fishingMinigameCatchSound: Sound,
 
 )
